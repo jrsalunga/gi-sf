a4a22b449ccee28d431598e4b05a5467aa44791e
Jefferson Salunga <jefferson.salunga@gmail.com>
2018-08-17 17:36:13 +0800

    fix

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cmd:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      
+      $dbf_file = 'C:\\GI_GLO\\SYSINFO.DBF';
+
+      if (file_exists($dbf_file)) { 
+        $db = dbase_open($dbf_file, 0);
+        $row = dbase_get_record_with_names($db, 1);
+
+        $code = trim($row['GI_BRCODE']);
+
+        dbase_close($db);
+        if(empty($code)) {
+          throw new Exception("Cannot locate Branch Code on backup");
+        }
+        else 
+          $this->info($code);
+      } else {
+        throw new Exception("Cannot locate SYSINFO"); 
+      }
+    }
+
+}
