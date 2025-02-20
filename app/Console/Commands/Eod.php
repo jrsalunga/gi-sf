213382701c4f4c325e985b842362c92f54ecc958
Jefferson Salunga <jefferson.salunga@yahoo.com>
2025-02-20 23:45:10 +0800

    fix

--- app/Console/Commands/Eod.php
+++ app/Console/Commands/Eod.php
@@ -5190,6 +5190,12 @@ class Eod extends Command
     $filename = $tid.$date->format('md');
     $fullpath = $src_path.DS.$filename.'.011';
 
+
+    $prev_tid = '1388';
+    if (!file_exists($fullpath) || file_exists($src_path.DS.$prev_tid.'.011')) 
+      $fullpath = $src_path.DS.$prev_tid.'.011';
+    
+
     $storage_dir = 'C:\RLC'.DS.'STORAGE'.DS.$date->format('Y');
 
     if (!is_dir($storage_dir))
