65de4c5242930d46ffbe511ce4b2464a4002b6eb
Jefferson Salunga <jefferson.salunga@yahoo.com>
2025-02-20 23:49:26 +0800

    fix

--- app/Console/Commands/Eod.php
+++ app/Console/Commands/Eod.php
@@ -4945,7 +4945,7 @@ class Eod extends Command
     $filename = $tid.$date->format('md');
     $fullpath = $src_path.DS.$filename.'.011';
 
-    $prev_tid = '1388';
+    $prev_tid = '1388'.$date->format('md');
     if (!file_exists($fullpath) || file_exists($src_path.DS.$prev_tid.'.011')) 
       $fullpath = $src_path.DS.$prev_tid.'.011';
 
